4bf6cdfb626b842118ee438f1d9cfb62aa7f509d
SuryaKavutarapu <suryakavutarapu@hotmail.com>
2016-12-14 13:05:37 +0530

    14 dec 2016 wrapper class methods Explained

--- WrapperClasses/WrapperClass.java
+++ WrapperClasses/WrapperClass.java
@@ -1,3 +1,12 @@
+
+import static java.lang.Integer.*;
+/**
+ *   note : Don't confuse with syntax  i'm using static import to use method name directly
+ * 
+ *    insted of using Integer.methodname;
+ *  
+ */
+
 class WrapperClass{
   public static void main(String []args){
     //# int wrapping and un wrapping
@@ -38,5 +47,41 @@ class WrapperClass{
     //both has two differnt values so we get false when use equals()
 System.out.println("\n================comparing objects with .equals method==================\n");
     System.out.println("Comparing objects using .equals method : "+x.equals(y));
+System.out.println("\n================Max and min final variables ===========================\n");
+    //## max value and Min value
+    Integer range = new Integer("1000");
+    System.out.println(range.MAX_VALUE);
+    System.out.println(range.MIN_VALUE);
+    
+   /**
+    *  valueOf method is similar  to parse method but there is some more additional functionality
+       and conversion we can pase sting value and we covert int Binary,Octal,Decimal and hexadecimal
+    */
+
+    System.out.println("\n================valueOf method explained ==========================\n");
+    //first type
+    Integer e = valueOf(10);//integer type  
+    //second type
+    Integer F = valueOf("100");//string passing and conveting into int
+    //third type
+    Integer octal =valueOf("1010",8);
+    Integer binary = valueOf("1010",2);
+    Integer decimal = valueOf("1010",10);
+    Integer hexadecimal = valueOf("1010",16);
+    System.out.println(octal+" : coverted 10 in octal form\n"+binary+" : coverted 10 to binary\n"+decimal+" : coverted 10 in decimal form\n"+hexadecimal+" : coverted 10 in hexadecimal form");
+    
+    System.out.println("\n================ toString  method==================================\n");
+    
+    //type 1 converting int to string
+    Integer tostring = valueOf("10");
+    
+    String Sdecimal = Integer.toString(tostring);
+    String Sbinary = toBinaryString(tostring);
+    String Shexadecimal = toHexString(tostring);
+    String Soctal = toOctalString(tostring);
+
+    System.out.println(Soctal+" : coverted 10 in octal form\n"+Sbinary+" : coverted 10 to binary\n"+Sdecimal+" : coverted 10 in decimal form\n"+Shexadecimal+" : coverted 10 in hexadecimal form");
+
+     
   }/*method main programm execution starts from here*/
 }/*class WrapperClass*/
